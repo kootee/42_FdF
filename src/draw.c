395fee16bcc258a8e7179844d75015da792e9b10
Katja Toivola <ktoivola@c2r6p11.hive.fi>
2024-05-03 16:21:38 +0300

    adding functions to draw

--- src/draw.c
+++ src/draw.c
@@ -6,11 +6,16 @@
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 13:46:57 by ktoivola          #+#    #+#             */
-/*   Updated: 2024/05/03 14:59:22 by ktoivola         ###   ########.fr       */
+/*   Updated: 2024/05/03 16:21:26 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static void plot(int16_t x, int16_t y, uint16_t alpha)
+{
+	// image [y , x] = put_colour;
+}
 
 static bool	is_steep(point_t start, point_t end)
 {
@@ -53,12 +58,31 @@ void	swap_values(point_t start, point_t end, int32_t *z, bool steep)
 	}
 }
 
-void	first_point(point_t start, point_t end, int32_t gradient)
+void	first_point(point_t start, point_t end, int32_t gradient, bool steep)
 {
 	/* Handle first point */
+	int32_t inter_y;
 	int32_t	x_end = (start.axis[X] + 0x8000) & 0xFFFF0000;
 	int32_t	y_end = start.axis[Y] + ((gradient * (x_end - start.axis[X])) >> 16);
 	int32_t	x_gap = 0x10000 - ((start.axis[X] + 0x8000) & 0xFFFF);
+	int16_t	xpxl1 = end.axis[X] >> 16;
+	int16_t ypxl1 = end.axis[Y] >> 16;
+	if (steep)
+	{
+		plot(ypxl1, xpxl1, 0x100 - (((0x100 - ((y_end >> 8) & 0xFF)) * x_gap) >> 16));
+		plot(ypxl1 + 1, xpxl1, 0x100 - ((((y_end >> 8) & 0xFF)  * x_gap) >> 16));
+	}
+	else
+	{
+		plot(xpxl1, ypxl1, 0x100 - (((0x100 - ((y_end >> 8) & 0xFF)) * x_gap) >> 16));
+		plot(xpxl1, ypxl1 + 1, 0x100 - ((((y_end >> 8) & 0xFF)  * x_gap) >> 16));
+	}
+	inter_y = y_end + gradient;
+}
+
+void	second_point(point_t start, point_t end, int32_t gradient, bool steep)
+{
+	
 }
 
 void	draw_wire(point_t start, point_t end)
@@ -83,4 +107,6 @@ void	draw_wire(point_t start, point_t end)
 		gradient = 0x10000;
 	else
 		gradient = (dy << 8) / (dx >> 8);
+
+	first_point(start, end, gradient, steep);
 }
